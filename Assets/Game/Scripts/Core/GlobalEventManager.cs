50a84964f263dd9fac793ff4f3b271306493c70a
Natallia Kazus <natalliakazus@gmail.com>
2023-07-14 19:20:02 +0300

    field objects placement

---
+++ Assets/Game/Scripts/Core/GlobalEventManager.cs
@@ -0,0 +1,66 @@
+using System;
+
+public static class GlobalEventManager
+{
+    public static event Action<bool> GameStateEvent;
+    public static event Action VibroEvent;
+    public static event Action RewardSoundEvent;
+    public static event Action BonusSoundEvent;
+    public static event Action<bool> VibroSettingsEvent;
+    public static event Action<bool> SoundSettingsEvent;
+
+    public static event Action<int> WinEvent;
+    public static event Action<CellIndices> MovePlayerEvent;
+    public static event Action LocksUpdateEvent;
+    public static event Action CoinEvent;
+
+    public static void SwitchGameState(bool activate)
+    {
+        GameStateEvent?.Invoke(activate);
+    }
+
+    public static void PlayVibro()
+    {
+        VibroEvent?.Invoke();
+    }
+
+    public static void PlayReward()
+    {
+        RewardSoundEvent?.Invoke();
+    }
+
+    public static void PlayBonus()
+    {
+        BonusSoundEvent?.Invoke();
+    }
+
+    public static void SetVibro(bool value)
+    {
+        VibroSettingsEvent?.Invoke(value);
+    }
+
+    public static void SetSound(bool value)
+    {
+        SoundSettingsEvent?.Invoke(value);
+    }
+
+    public static void DoWin(int value)
+    {
+        WinEvent?.Invoke(value);
+    }
+
+    public static void MovePlayer(CellIndices cell)
+    {
+        MovePlayerEvent?.Invoke(cell);
+    }
+
+    public static void UpdateLocks()
+    {
+        LocksUpdateEvent?.Invoke();
+    }
+
+    public static void CollectCoin()
+    {
+        CoinEvent?.Invoke();
+    }
+}
---
+++ Assets/Game/Scripts/ScoreManager.cs
@@ -0,0 +1,51 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private Text globalScoreUI;
+    [SerializeField] private Text movesUI;
+    [SerializeField] private Text jumpsUI;
+    [SerializeField] private Text unlocksUI;
+
+    [SerializeField] private Image jumpsImage;
+    [SerializeField] private Sprite jumpsOn;
+    [SerializeField] private Sprite jumpsOff;
+    [SerializeField] private Image locksImage;
+    [SerializeField] private Sprite locksOn;
+    [SerializeField] private Sprite locksOff;
+
+
+    public void UpdateValues(int id, int value)
+    {
+        Text ui;
+        switch (id)
+        {
+            case 2: ui = movesUI; break;
+            case 3: ui = jumpsUI; CheckValue(value, true); break;
+            case 4: ui = unlocksUI; CheckValue(value, false); break;
+            default: ui = globalScoreUI; break;
+        }
+
+        UpdateText(ui, value.ToString());
+    }
+
+    private void CheckValue(int value, bool jumps)
+    {
+        Image targetImage = jumps ? jumpsImage : locksImage;
+        if(value <= 0)
+        {
+            targetImage.sprite = jumps ? jumpsOff : locksOff;
+        }
+        else
+        {
+            targetImage.sprite = jumps ? jumpsOn : locksOn;
+        }
+    }
+
+    private void UpdateText(Text uiText, string value)
+    {
+        uiText.DOText(value, 0.5f).Play();
+    }
+}
